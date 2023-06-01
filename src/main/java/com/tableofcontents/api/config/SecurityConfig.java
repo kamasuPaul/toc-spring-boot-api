636ca073a337fd08d1d54e4c9f0a80819d74c319
Kamasu Paul <kamasupaul@gmail.com>
2023-06-01 11:31:40 +0300

    -- add token validation

---
+++ src/main/java/com/tableofcontents/api/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.tableofcontents.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .cors()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/error")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                // Enable JWT Authentication
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .build();
+    }
+
+}
